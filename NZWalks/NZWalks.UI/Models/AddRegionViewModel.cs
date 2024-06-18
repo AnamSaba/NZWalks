b7a282c68e27540bb9557081a20542be2209b1d1
anamsaba18 <anam.saba378@gmail.com>
2024-06-18 15:24:33 -0700

    Created ASP.NET MVC Web application to consume Web APIs

---
+++ NZWalks/NZWalks.UI/Models/AddRegionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.UI.Models
+{
+	public class AddRegionViewModel
+	{
+		public string Code { get; set; }
+		public string Name { get; set; }
+		public double Area { get; set; }
+		public double Lat { get; set; }
+		public double Long { get; set; }
+		public long Population { get; set; }
+		public string? RegionImageUrl { get; set; }
+	}
+}
